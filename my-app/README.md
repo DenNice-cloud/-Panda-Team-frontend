5faa23c636859f50965d2d3269ab284792c38e16
Denis Kotov <dionisiy2904@gmail.com>
2024-08-19 23:02:18 +0300

    add first site design and small logic

---
+++ my-app/README.md
@@ -0,0 +1,1 @@
+Clear .env file?
---
+++ my-app/src/utils/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API_KEY = "c4b4cf0d1a560593f9339800a41e4f81";
+
+export const fetchWeatherByCity = (city) => {
+  return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+};
+
+export const fetchWeatherByCityForWeek = (city) => {
+  return axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`);
+};
